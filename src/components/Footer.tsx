902eb9a1b2dd4574be297d3716a705079101d1be
Samson Sham <samsonsham@gmail.com>
2022-04-13 13:13:58 +0100

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <></>;
-};
+  const today = new Date()
+  const year = today.getFullYear()
+  return (
+    <footer>
+      <p>&copy; {year} Samson Sham</p>
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
