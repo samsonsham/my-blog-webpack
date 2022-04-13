51f6dd8fa714e15e9eca3268d68c9f42441f1fb7
Samson Sham <samsonsham@gmail.com>
2022-04-13 14:19:29 +0100

    update workflow

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: path.join(__dirname, 'src/index.tsx'),
   output: {
-    path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
     publicPath: '/',
   },
